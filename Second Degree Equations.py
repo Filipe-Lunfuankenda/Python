0fca96def556f29f13d648dd660a091c9524de94
Filipe Lunfuankenda <135343917+Filipe-Lunfuankenda@users.noreply.github.com>
2023-09-26 09:19:53 +0100

    Update Second Degree Equations.py

--- Second Degree Equations.py
+++ Second Degree Equations.py
@@ -1,14 +1,14 @@
 
-a = float(input("Digite o valor de a:"))
-b = float(input("Digite o valor de b:")) 
-c = float(input("Digite o valor de c:"))
+a = float(input("Digite o valor de a: "))
+b = float(input("Digite o valor de b: ")) 
+c = float(input("Digite o valor de c: "))
 
 delta = b**2 - 4*a*c
 raiz_delta = delta**(1/2)
 
 print ("-"*60)
 print ("O valor de delta é", delta)
-print ("A raiz quadrada de", delta, "é", raiz_delta)
+print (f"A raiz quadrada de {delta} é {raiz_delta}")
 
 x1 = (-b + raiz_delta)/2*a
 x2 = (-b - raiz_delta)/2*a
@@ -16,15 +16,15 @@ x2 = (-b - raiz_delta)/2*a
 print ("-"*60)
 if delta > 0:
     print ("A equação possui duas raízes diferentes")
-    print ("O valor de x1 é", x1)
-    print ("O valor de x2 é", x2)
+    print ("O valor de x1 é ", x1)
+    print ("O valor de x2 é ", x2)
 elif delta == 0:
     print ("A equação possui uma única raiz igual")
-    print ("O valor de x1 é", x1)
-    print ("O valor de x2 é", x2)
+    print ("O valor de x1 é ", x1)
+    print ("O valor de x2 é ", x2)
 else:
     print ("Delta negativo, logo não possui raiz")
-    print ("A raiz quadrada de", delta, "não existe no conjunto de números reais")
+    print (f"A raiz quadrada de {delta} não existe no conjunto de números reais")
 
 
 
