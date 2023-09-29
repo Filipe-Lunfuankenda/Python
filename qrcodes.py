0154ae57990e61a8ebbb6914fb8f69b1ec099216
Filipe Lunfuankenda <135343917+Filipe-Lunfuankenda@users.noreply.github.com>
2023-09-29 17:51:59 +0100

    Update qrcodes.py

--- qrcodes.py
+++ qrcodes.py
@@ -1,8 +1,9 @@
+# Generate a QR CODE at any time. You just need to have the module 'qrcode' installed
 import qrcode as qr
 
-print("Seja Bem-vindo(a)! Este programa o ajudará a criar um código QR do endereço que você quiser.")
-url = input("Introduza o endereço ou URL para o Código QR:")
-salvo = input("Que nome deseja dar a imagem?")
+print("Este programa o ajudará a criar um código QR do endereço que você quise!")
+url = input("Introduza o endereço ou URL para o Código QR: ")
+salvo = input("Que nome deseja dar a imagem? ")
 imagemCodigo = qr.make(url)
 imagemCodigo.save(salvo+".png")
-print("O seu Código QR foi criado e salvo com sucesso!")
+print(f"O seu Código QR foi criado e salvo com sucesso com o nome {salvo}!")
