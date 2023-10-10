e46b73024bb1ff527a568135230af8433ed42fdb
Filipe Lunfuankenda <135343917+Filipe-Lunfuankenda@users.noreply.github.com>
2023-10-10 16:11:04 +0100

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -1,4 +1,4 @@
-#Encriptar qualquer arquivo selecionado usando a Linguagem Python com as bibliotecas criptography, PySimpleGUI e os
+#Encriptar/Criptografar qualquer arquivo selecionado usando a Linguagem Python com as bibliotecas criptography, PySimpleGUI e os
 import PySimpleGUI as sg
 from cryptography.fernet import Fernet
 import os
